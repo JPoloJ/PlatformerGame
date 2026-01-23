6e1ffa2bd4636140c5a31b9128d7c55b160ee95d
maarcab <maragonesb@edu.tecnocampus.cat>
2026-01-23 15:22:55 +0100

    Animations
    New animations, new items, new world assets

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Coin : MonoBehaviour
 {
@@ -11,11 +9,35 @@ public class Coin : MonoBehaviour
 
     public static Action<Coin> OnCoinCollected;
 
-    
+    private Animator _animator;
+    private Collider2D _collider;
+    private bool _hasBeenCollected = false;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _collider = GetComponent<Collider2D>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-       
+        if (other.CompareTag("Player") && !_hasBeenCollected)
+        {
+            _hasBeenCollected = true;
+            StartCoroutine(CollectSequence());
+        }
+    }
+    private IEnumerator CollectSequence()
+    {
         OnCoinCollected?.Invoke(this);
+
+        if (_collider != null) _collider.enabled = false;
+
+        if (_animator != null)
+        {
+            _animator.SetTrigger("Collected");
+        }
+
+        yield return new WaitForSeconds(0.5f);
 
         Destroy(gameObject);
     }
