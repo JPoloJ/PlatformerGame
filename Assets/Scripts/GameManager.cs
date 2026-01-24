336110765d8a868efe468e284da7eb7322ba4e38
JPoloJ <jpoloj@edu.tecnocampus.cat>
2026-01-24 17:37:44 +0100

    Esc for titlescreen & enter for restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public int finalScore;
+
     private void Awake()
     {
         //Singleton setUp
@@ -16,7 +19,47 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         //End of Singleton
     }
-    public int finalScore;
+
+    private void Update()
+    {
+        HandleGlobalInput();
+    }
+
+    private void HandleGlobalInput()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame || Keyboard.current.numpadEnterKey.wasPressedThisFrame)
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+
+            if (currentScene == "Title")
+            {
+                OnStart();
+            }
+            else if (currentScene == "Gameplay")
+            {
+                OnRestart();
+            }
+            else if (currentScene == "Ending")
+            {
+                OnRestart();
+            }
+        }
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+
+            if (currentScene == "Title")
+            {
+                OnExit();
+            }
+            else if (currentScene == "Gameplay" || currentScene == "Ending")
+            {
+                ReturnToTitle();
+            }
+        }
+    }
+
     public void OnStart()
     {
         if (SceneManager.GetActiveScene().name == "Title")
@@ -29,6 +72,18 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene("Title");
         }
     }
+
+    public void OnRestart()
+    {
+        finalScore = 0;
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
     public void OnExit()
     {
         Debug.Log("Exited");
