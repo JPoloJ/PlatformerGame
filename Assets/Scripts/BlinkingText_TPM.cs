193f2f4a93fed88cc27398887f3f15610e028da4
JPoloJ <jpoloj@edu.tecnocampus.cat>
2026-01-24 18:45:57 +0100

    UI improved

---
+++ Assets/Scripts/BlinkingText_TPM.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+
+public class BlinkingText_TMP : MonoBehaviour
+{
+    [SerializeField] private float blinkSpeed = 0.5f;
+    [SerializeField] private bool startBlinking = true;
+
+    private TextMeshProUGUI textComponent;
+    private float timer;
+
+    void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        timer = 0f;
+    }
+
+    void Update()
+    {
+        if (!startBlinking) return;
+
+        timer += Time.deltaTime;
+
+        if (timer >= blinkSpeed)
+        {
+            textComponent.enabled = !textComponent.enabled;
+            timer = 0f;
+        }
+    }
+
+    public void StartBlinking()
+    {
+        startBlinking = true;
+        textComponent.enabled = true;
+    }
+
+    public void StopBlinking()
+    {
+        startBlinking = false;
+        textComponent.enabled = true;
+    }
+}
